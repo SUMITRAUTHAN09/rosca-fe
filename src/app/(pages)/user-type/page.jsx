e34ff76126bf2743ebb92e970a140528e3250c2b
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-11-30 02:49:22 +0530

    added usertype page

--- src/app/(pages)/user-type/page.jsx
+++ src/app/(pages)/user-type/page.jsx
@@ -55,7 +55,7 @@ export default function UserTypePage() {
         }
       }
 
-      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/update-user-type`;
+      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/update-user-type`;
       console.log('🔍 Making PATCH request to:', url);
       console.log('🔍 Request body:', { userType: selectedType });
 
